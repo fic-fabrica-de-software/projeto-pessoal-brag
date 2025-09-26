01741ac337a3a2c998f8559f6e26ac9954b5d388
Braian-de-Liz <delizbraian@gmail.com>
2025-09-26 14:47:06 -0300

    melhoras

--- Assets/Scripts/Core/ObjectPool.cs
+++ Assets/Scripts/Core/ObjectPool.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 public class ObjectPool : MonoBehaviour
 {
     [Header("Object Pool Settings")]
-    [SerializeField] private GameObject prefab; // Prefab a ser instanciado no pool
-    [SerializeField] private int poolSize = 10; // Tamanho inicial do pool
+    [SerializeField] private GameObject prefab; 
+    [SerializeField] private int poolSize = 10; 
 
     private Queue<GameObject> pool = new Queue<GameObject>();
 
     private void Awake()
     {
-        // Inicializa o pool com objetos desativados
         for (int i = 0; i < poolSize; i++)
         {
             GameObject obj = Instantiate(prefab);
@@ -20,7 +19,7 @@ public class ObjectPool : MonoBehaviour
         }
     }
 
-    // Obtém um objeto do pool
+   
     public GameObject GetObject(Vector3 position, Quaternion rotation)
     {
         if (pool.Count > 0)
@@ -33,13 +32,13 @@ public class ObjectPool : MonoBehaviour
         }
         else
         {
-            // Se o pool estiver vazio, instancia um novo objeto
+            
             GameObject obj = Instantiate(prefab, position, rotation);
             return obj;
         }
     }
 
-    // Retorna um objeto ao pool
+
     public void ReturnObject(GameObject obj)
     {
         obj.SetActive(false);
